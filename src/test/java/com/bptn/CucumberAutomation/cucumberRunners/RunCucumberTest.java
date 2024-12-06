a34deb605e95d1e8642a66dfeee439f406b66119
your-name <abdellamahmoud98@gmail.com>
2024-12-06 10:21:38 -0500

    added 12.14: Login Feature Implementation for Web Application

--- src/test/java/com/bptn/CucumberAutomation/cucumberRunners/RunCucumberTest.java
+++ src/test/java/com/bptn/CucumberAutomation/cucumberRunners/RunCucumberTest.java
@@ -5,17 +5,17 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("com/bptn/CucumberAutomation/features/example.feature")
+//@SelectClasspathResource("com/bptn/CucumberAutomation/features/example.feature")
 //@SelectClasspathResource("com/bptn/CucumberAutomation/features/calculator.feature")
 //@SelectClasspathResource("com/bptn/CucumberAutomation/features/strongPassword.feature")
 //@SelectClasspathResource("com/bptn/CucumberAutomation/features/signUp.feature")
 //@SelectClasspathResource("com/bptn/CucumberAutomation/features/login.feature")
 //@SelectClasspathResource("com/bptn/CucumberAutomation/features/shoppingCart.feature")
+@SelectClasspathResource("com/bptn/CucumberAutomation/features/loginPage.feature")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.bptn.CucumberAutomation")
 public class RunCucumberTest {
 }
